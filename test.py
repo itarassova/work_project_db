5a7942b2fa9deb125cb3a759416fbcf89d91b082
Irina Tarasova <somatotrophin@gmail.com>
2021-03-12 23:10:47 +0000

    Co-authored-by: Dmitri Smirnov <smirnov@users.noreply.github.com>

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ from jsonpath_ng import jsonpath
 from jsonpath_ng.ext import parser
 from openpyxl import Workbook, load_workbook
 import time
+import logging as log
+
 
 start = time.time()
 
@@ -17,35 +19,26 @@ export_worksheet = export_workbook.active
 
 row = 2
 
-#list_cas = []
+def get_room_from_location(location):
+    location_split = location.split('>')[1]
+    substring = ' - '
+    if substring in location_split:
+        room = location_split.split('-')[1]
+    else:
+        room = location_split.split()[1]
+    return room
 
-#for col in ws['B']:
-    #list_cas.append(col.value)
-
-#substance_name_list = []
-
-#for col in ws['A']:
-    #substance_name_list.append(col.value)
-
-#row_substance = 2
-#for substance in substance_name_list:
-    #export_worksheet['G'+ str(row_substance)] = str(substance)
-    #row_substance+=1
-
-#location_list = []
-
-#for col in ws['H']:
-    #location_list.append(col.value)
-
-#row_location = 2
-#for location in location_list:
-    #room_location = location.split('>')[1]
-    #print(room_location)
-    #xport_worksheet['H'+ str(row_location)] = str(room_location)
-    #row_location+=1
-
-
-
+def convert_quantity_units(amount, unit):
+    mass_unit_mg = 'mg'
+    mass_unit_kg = 'Kg'
+    if unit == mass_unit_mg:
+        converted_amount = int(amount) * 0.001
+    if unit == mass_unit_kg:
+        converted_amount = int(amount) * 1000
+    volume_unit_L = 'L'
+    if unit == volume_unit_L:
+        converted_amount = int(amount) * 1000
+    return converted_amount
 
 
 for cell in ws['B']:
@@ -54,7 +47,7 @@ for cell in ws['B']:
         cid_url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{}/cids/JSON".format(cas)
         cid_resp = requests.get(url=cid_url)
         cid = cid_resp.json()["IdentifierList"]["CID"][0]
-        print(cid)
+        log.info(cid)
         msds_url = "https://pubchem.ncbi.nlm.nih.gov/compound/{}#datasheet=LCSS".format(cas)
         compound_url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/{}/JSON".format(cid)
         resp = requests.get(url=compound_url)
@@ -65,7 +58,7 @@ for cell in ws['B']:
         environmental_warning_lines = []
         other_hazards = []
         for warning_line in filter_expression.find(data):
-            print(warning_line.value)        
+            log.info(warning_line.value)        
             if warning_line.value[1] == '2':
                 physical_warning_lines.append(warning_line.value)
             elif warning_line.value[1] == '3':
@@ -74,7 +67,6 @@ for cell in ws['B']:
                 environmental_warning_lines.append(warning_line.value)
             else:
                 other_hazards.append(warning_line.value)
-            
 
         physical_hazards = '\n'.join(physical_warning_lines)
         health_hazards = '\n'.join(health_warning_lines)
@@ -87,25 +79,15 @@ for cell in ws['B']:
         export_worksheet['D'+ str(row)] = str(environmental_hazards)
         export_worksheet['E'+ str(row)] = str(other_hazards)
         export_worksheet['F'+ str(row)] = str(msds_url)
-        export_worksheet['G'+ str(row)] = str(ws['A'+str(row-1)].value)
-        export_worksheet['H'+ str(row)] = str(ws['H'+str(row-1)].value).split('>')[1]
-        
-        
-
-        
-
-        
-
-
-
-
-
-
-        
+        export_worksheet['G'+ str(row)] = str(ws['A'+str(row)].value)
+        room_location = get_room_from_location(ws['H'+str(row)].value)
+        export_worksheet['H'+ str(row)] = str(room_location)
+        compound_unit = ['B'+ str(row)]
         row+=1
 
-    except:
+    except Exception as e:
         print(str(cas) + ' didn\'t work')
+        log.error(e, exc_info=True)
 
 export_workbook.save(filename="test_output.xlsx")
 end = time.time()
